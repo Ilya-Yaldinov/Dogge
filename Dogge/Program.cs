3a693fff8f9d87ec59b6e4b5529e51de84765113
Ilya-Yaldinov <90057250+Ilya-Yaldinov@users.noreply.github.com>
2022-02-18 17:57:20 +0500

    Добавьте файлы проекта.

---
+++ Dogge/Program.cs
@@ -0,0 +1,68 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        ConsoleWorker.WriteAt(@"D:/Dog.csv");
+    }
+}
+
+class ConsoleWorker
+{
+    public static void WriteAt(string path)
+    {
+        int x = Console.CursorTop;
+        int y = Console.CursorLeft;
+        bool tailRelease = false;
+        var file = FileWorker.FileReader(path);
+        for (int i = 0; i < file.Length; i++)
+        {
+            switch (file[i])
+            {
+                case "Right":
+                    Console.SetCursorPosition(x++, y);
+                    if(tailRelease == true)
+                    {
+                        Console.Write("█");
+                    }
+                    break;
+                case "Left":
+                    Console.SetCursorPosition(x--, y);
+                    if (tailRelease == true)
+                    {
+                        Console.Write("█");
+                    }
+                    break;
+                case "Up":
+                    Console.SetCursorPosition(x, y--);
+                    if (tailRelease == true)
+                    {
+                        Console.Write("█");
+                    }
+                    break;
+                case "Down":
+                    Console.SetCursorPosition(x, y++);
+                    if (tailRelease == true)
+                    {
+                        Console.Write("█");
+                    }
+                    break;
+                case "TailRelease":
+                    tailRelease = false;
+                    break;
+                case "TailRaise":
+                    tailRelease = true;
+                    break;
+            }
+        }
+        Console.ReadKey();
+    }
+}
+
+class FileWorker
+{
+    public static string[] FileReader(string path)
+    {
+        string[] file = File.ReadAllLines(path);
+        return file;
+    }
+}
